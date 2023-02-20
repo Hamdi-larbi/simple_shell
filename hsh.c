aae3f409ec2078bd1c619eaa1f838a0905f76918
Hamdi-larbi <larbi.hamdi@gmail.com>
2023-02-20 13:30:49 -0800

    update shell 0.1 handle EOF

--- hsh.c
+++ hsh.c
@@ -24,37 +24,41 @@ int main(void)
 			exit(1);
 		}
 		i = _getline(&lineptr, &size, stdin);
-		if (i != -1)
-			lineptr[i - 1] = '\0';
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			args[0] = lineptr;
-			args[1] = NULL;
-			if (execve(args[0], args, environ) == -1)
-			{
-				perror("./hsh ");
-				exit(EXIT_FAILURE);
-			}
-		}
+		if (i == -1)
+			break;
+		lineptr[i - 1] = '\0';
+		if (strlen(lineptr) == 0)
+			continue;
 		else
 		{
-			if (wait(&status) == -1)
+			pid = fork();
+			if (pid == -1)
 			{
-				perror("wait");
+				perror("fork");
 				exit(EXIT_FAILURE);
 			}
+			else if (pid == 0)
+			{
+				args[0] = lineptr;
+				args[1] = NULL;
+				if (execve(args[0], args, environ) == -1)
+				{
+					perror("./hsh ");
+					exit(EXIT_FAILURE);
+				}
+			}
+			else
+			{
+				if (wait(&status) == -1)
+				{
+					perror("wait");
+					exit(EXIT_FAILURE);
+				}
+			}
+			if (strcmp(lineptr, "exit") == 0)
+				break;
+			free(lineptr);
 		}
-
-		if (strcmp(lineptr, "exit") == 0)
-			break;
-		free(lineptr);
 	}
-
 	return (0);
 }
