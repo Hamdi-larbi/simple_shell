a6b06cd6c310df1e94b386a5b2493d3af0016562
Hamdi-larbi <larbi.hamdi@gmail.com>
2023-02-24 16:20:26 -0800

    update the function _strtok

--- _strtok.c
+++ _strtok.c
@@ -38,42 +38,59 @@ char **_strtok(char *str, const char *delim)
 				/*test if there is a much withe the delimiter*/
 			{
 				temp[l] = '\0';
-				/*terminate the temporary string with the null caracter*/
-				ptr[k] = malloc((l + 1) * sizeof(char));
+				/*termiate the temporary string with the null caracter*/
+				if (temp[0] != '\0')
+				{
+					ptr[k] = malloc((l + 1) * sizeof(char));
 				/*allocate the memory for the k th string*/
-				if (ptr[k] == NULL)
-				{
-					printf("ERROR: Memory allocation failed\n");
-					exit(1);
-				}
-				for (m = 0; temp[m] != '\0'; m++)
-					/*copy the string from the temporary to the k th ptr array*/
-				{
-					ptr[k][m] = temp[m];
-				}
-				ptr[k++][m] = '\0';
-				if (k >= ptr_size)
-					/*test if the memory allocated to ptr still fit*/
-				{
-					ptr_size *= 2;
-					ptr = realloc(ptr, ptr_size * sizeof(char *));
-					if (ptr == NULL)
+					if (ptr[k] == NULL)
 					{
 						printf("ERROR: Memory allocation failed\n");
 						exit(1);
 					}
+					for (m = 0; temp[m] != '\0'; m++)
+					/*copy the string from the temporary to the k th ptr array*/
+					{
+						ptr[k][m] = temp[m];
+					}
+					ptr[k++][m] = '\0';
+					if (k >= ptr_size)
+					/*test if the memory allocated to ptr still fit*/
+					{
+						ptr_size *= 2;
+						ptr = realloc(ptr, ptr_size * sizeof(char *));
+						if (ptr == NULL)
+						{
+							printf("ERROR: Memory allocation failed\n");
+							exit(1);
+						}
+					}
+					free(temp);
+					/*free temp and then reallocate it to empty the temp string*/
+					temp = malloc((strlen(str) + 2 - l));
+					if (temp == NULL)
+					{
+						printf("ERROR: Memory allocation failed\n");
+						exit(1);
+					}
+					l = 0;
+					break;
+					/*break if there is a much*/
 				}
-				free(temp);
-				/*free temp and then reallocate it to empty the temp string*/
-				temp = malloc((strlen(str) + 2 - l));
-				if (temp == NULL)
+				else
 				{
-					printf("ERROR: Memory allocation failed\n");
-					exit(1);
+					free(temp);
+					/*free temp and then reallocate it to empty the temp string*/
+					temp = malloc((strlen(str) + 2 - l));
+					if (temp == NULL)
+					{
+						printf("ERROR: Memory allocation failed\n");
+						exit(1);
+					}
+					l = 0;
+					break;
+					/*break if there is a much*/
 				}
-				l = 0;
-				break;
-				/*break if there is a much*/
 			}
 		}
 		if (delim[j] == '\0')
@@ -83,20 +100,27 @@ char **_strtok(char *str, const char *delim)
 		}
 	}
 	temp[l] = '\0';
-	ptr[k] = malloc((l + 1) * sizeof(char));
+	if (temp[0] != '\0')
+	{
+		ptr[k] = malloc((l + 1) * sizeof(char));
 	/*allocate the memory to the final string*/
-	if (ptr[k] == NULL)
-	{
-		printf("ERROR: Memory allocation failed\n");
-		exit(1);
-	}
-	for (n = 0; temp[n] != '\0'; n++)
+		if (ptr[k] == NULL)
+		{
+			printf("ERROR: Memory allocation failed\n");
+			exit(1);
+		}
+		for (n = 0; temp[n] != '\0'; n++)
 		/*copy the final string from the temp*/
-	{
-		ptr[k][n] = temp[n];
+		{
+			ptr[k][n] = temp[n];
+		}
+		ptr[k++][n] = '\0';
+		ptr[k] = NULL;
 	}
-	ptr[k++][n] = '\0';
-	ptr[k] = NULL;
+	else
+	{
+		ptr[k] = NULL;
+	}
 	free(temp);
 	return (ptr);
 }
